b311ebaae009875c3b2535748d342a91b737076d
harshithh-18 <harshith.gottipati2007@gmail.com>
2025-08-25 22:41:58 +0530

    Update textsummarization.py

--- models/textsummarization.py
+++ models/textsummarization.py
@@ -2,9 +2,17 @@ import google.generativeai as genai
 from google import genai
 
 client = genai.Client(api_key="AIzaSyAH6IMECeM07yMeeGwfhnnRsue0A4Xt2Y4")
+def summarize(email):
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="summarise: A lone wolf howled at the moon, its voice echoing through the silent forest. A small rabbit twitched its nose, startled by the sound. The wolf, sensing its prey, began to stalk forward, muscles tensing. But then, a gentle breeze carried the scent of wildflowers, and the wolf, distracted, turned away, leaving the rabbit unharmed. "
-)
-print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        email_text=email,
+        conetnts=f"""
+          Extract event details from the email and return JSON with:
+          "title", "date", "start_time", "end_time", "location", "description".
+
+          Email:
+            \"\"\"{email_text}\"\"\"
+            """
+    )
+    return response.text
